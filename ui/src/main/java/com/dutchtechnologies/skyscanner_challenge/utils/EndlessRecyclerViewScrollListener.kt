1831477389cd9e561abf2411409383da8108651e
william.gouvea <wlliam.cruz.gouvea@gmail.com>
2019-03-20 04:00:07 -0300

    -Adjust params at EndlessScrollListener

--- ui/src/main/java/com/dutchtechnologies/skyscanner_challenge/utils/EndlessRecyclerViewScrollListener.kt
+++ ui/src/main/java/com/dutchtechnologies/skyscanner_challenge/utils/EndlessRecyclerViewScrollListener.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
  abstract class EndlessRecyclerViewScrollListener : RecyclerView.OnScrollListener {
     // The minimum amount of items to have below your current scroll position
     // before loading more.
-    private var visibleThreshold = 10
+    private var visibleThreshold = 25
     // The current offset index of data you have loaded
     private var currentPage = 1
     // The total number of items in the dataset after the last load
